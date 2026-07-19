39fa4060c0727ee543e178f51e5eda73dc9024f1
ChaldS <ttserafim9@gmail.com>
2026-07-19 13:51:23 +0700

    patched limboapi

---
+++ api/src/main/java/net/elytrium/limboapi/api/Limbo.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2021 - 2025 Elytrium
+ *
+ * The LimboAPI (excluding the LimboAPI plugin) is licensed under the terms of the MIT License. For more details,
+ * reference the LICENSE file in the api top-level directory.
+ */
+
+package net.elytrium.limboapi.api;
+
+import com.velocitypowered.api.command.Command;
+import com.velocitypowered.api.command.CommandMeta;
+import com.velocitypowered.api.proxy.Player;
+import java.util.function.Supplier;
+import net.elytrium.limboapi.api.command.LimboCommandMeta;
+import net.elytrium.limboapi.api.player.GameMode;
+import net.elytrium.limboapi.api.protocol.PacketDirection;
+import net.elytrium.limboapi.api.protocol.packets.PacketMapping;
+
+public interface Limbo {
+
+  void spawnPlayer(Player player, LimboSessionHandler handler);
+
+  void respawnPlayer(Player player);
+
+  long getCurrentOnline();
+
+  Limbo setName(String name);
+
+  Limbo setReadTimeout(int millis);
+
+  Limbo setWorldTime(long ticks);
+
+  Limbo setGameMode(GameMode gameMode);
+
+  Limbo setShouldRejoin(boolean shouldRejoin);
+
+  Limbo setShouldRespawn(boolean shouldRespawn);
+
+  @Deprecated
+  Limbo setShouldUpdateTags(boolean shouldUpdateTags);
+
+  Limbo setReducedDebugInfo(boolean reducedDebugInfo);
+
+  Limbo setViewDistance(int viewDistance);
+
+  Limbo setSimulationDistance(int simulationDistance);
+
+  Limbo setMaxSuppressPacketLength(int maxSuppressPacketLength);
+
+  Limbo registerCommand(LimboCommandMeta commandMeta);
+
+  Limbo registerCommand(CommandMeta commandMeta, Command command);
+
+  Limbo registerPacket(PacketDirection direction, Class<?> packetClass, Supplier<?> packetSupplier, PacketMapping[] packetMappings);
+
+  void dispose();
+}
